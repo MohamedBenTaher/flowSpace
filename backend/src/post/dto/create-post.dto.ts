bc6078b4a790909524db275a7e3c16bddb769801
Mohamed Ben Taher <mbentaher1998@gmail.com>
2023-03-28 11:29:28 +0000

    frontend next ts stetup

--- backend/src/post/dto/create-post.dto.ts
+++ backend/src/post/dto/create-post.dto.ts
@@ -1,12 +1,7 @@
-import { CreateUserDto } from "src/user/dto/create-user.dto";
-
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 export class CreatePostDto {
-
-    id:Number;
-    title:string;
-    content:string
-    published:boolean;
-    author:CreateUserDto;
-    authorId:number
+  title: string;
+  content: string;
+  published: boolean;
 }
