1c4b089417bdf3b618b7e133d1d82b8e2bc3c00a
Mohamed Ben Taher <mbentaher1998@gmail.com>
2023-03-28 11:57:55 +0000

    basic post crud

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
+  constructor(private readonly prisma: PrismaService) {}
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return this.prisma.post.create({ data: createPostDto });
   }
 
   findAll() {
-    return `This action returns all post`;
+    return this.prisma.post.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} post`;
+    return this.prisma.post.findUniqueOrThrow({
+      where: {
+        id: id,
+      },
+    });
   }
 
   update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+    return this.prisma.user.update({
+      where: { id },
+      data: updatePostDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} post`;
+    return this.prisma.post.delete({
+      where: {
+        id: id,
+      },
+    });
   }
 }
