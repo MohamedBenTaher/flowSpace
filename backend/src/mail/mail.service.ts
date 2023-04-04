a4c09278901f73a396aca383e59810a1bbc2cbc0
Mohamed Ben Taher <mbentaher1998@gmail.com>
2023-04-04 19:58:17 +0000

    mail basic conf

--- backend/src/mail/mail.service.ts
+++ backend/src/mail/mail.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMailDto } from './dto/create-mail.dto';
 import { UpdateMailDto } from './dto/update-mail.dto';
-
+import { MailerService } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { MailData } from './interfaces/mail-data.interface';
 @Injectable()
 export class MailService {
-  create(createMailDto: CreateMailDto) {
-    return 'This action adds a new mail';
+  constructor(
+    private mailerService: MailerService,
+    private configService: ConfigService,
+  ) {}
+
+  async userSignUp(mailData: MailData<{ hash: string }>) {
+    await this.mailerService.sendMail({
+      to: mailData.to,
+      subject: 'common.confirmEmail',
+      text: `${this.configService.get('app.frontendDomain')}/confirm-email/${
+        mailData.data.hash
+      } ${'common.confirmEmail'}`,
+      template: 'activation',
+      context: {
+        title: 'common.confirmEmail',
+        url: `${this.configService.get('app.frontendDomain')}/confirm-email/${
+          mailData.data.hash
+        }`,
+        actionTitle: 'common.confirmEmail',
+        app_name: this.configService.get('app.name'),
+        text1: 'confirm-email.text1',
+        text2: 'confirm-email.text2',
+        text3: 'confirm-email.text3',
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all mail`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} mail`;
-  }
-
-  update(id: number, updateMailDto: UpdateMailDto) {
-    return `This action updates a #${id} mail`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} mail`;
+  async forgotPassword(mailData: MailData<{ hash: string }>) {
+    await this.mailerService.sendMail({
+      to: mailData.to,
+      subject: 'common.resetPassword',
+      text: `${this.configService.get('app.frontendDomain')}/password-change/${
+        mailData.data.hash
+      } {'common.resetPassword')}`,
+      template: 'reset-password',
+      context: {
+        title: 'common.resetPassword',
+        url: `${this.configService.get('app.frontendDomain')}/password-change/${
+          mailData.data.hash
+        }`,
+        actionTitle: 'common.resetPassword',
+        app_name: this.configService.get('app.name'),
+        text1: 'reset-password.text1',
+        text2: 'reset-password.text2',
+        text3: 'reset-password.text3',
+        text4: 'reset-password.text4',
+      },
+    });
   }
 }
